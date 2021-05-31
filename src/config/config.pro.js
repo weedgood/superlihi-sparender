1e200fdcfcc418517a5098864b21e0d6f95e2b8b
Falcon <endless640c@gmail.com>
2021-05-31 12:15:11 +0800

    調整參數測試

--- src/config/config.pro.js
+++ src/config/config.pro.js
@@ -21,12 +21,12 @@ module.exports = {
     }
   },
   puppeteer: {
-    max: 8,//最多产生多少个 puppeteer 实例 。如果你设置它，请确保 在引用关闭时调用清理池。 pool.drain().then(()=>pool.clear())
+    max: 4,//最多产生多少个 puppeteer 实例 。如果你设置它，请确保 在引用关闭时调用清理池。 pool.drain().then(()=>pool.clear())
     min: 1,//保证池中最少有多少个实例存活
-    maxUses: 2048,//每一个 实例 最大可重用次数，超过后将重启实例。0表示不检验
+    maxUses: 1024,//每一个 实例 最大可重用次数，超过后将重启实例。0表示不检验
     testOnBorrow: true,// 在将 实例 提供给用户之前，池应该验证这些实例。
     autostart: false,//是不是需要在 池 初始化时 初始化 实例
-    idleTimeoutMillis: 1000 * 60 * 60,//如果一个实例 60分钟 都没访问就关掉他
+    idleTimeoutMillis: 1000 * 60 * 6,//如果一个实例 60分钟 都没访问就关掉他
     evictionRunIntervalMillis: 1000 * 60 * 3,//每 3分钟 检查一次 实例的访问状态
     start: {
       headless: true,
